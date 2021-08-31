4fe718eef9d380f4ef850aa280336dfced3ea609
Christopher Nguyen <89357853+christophernguyen6089@users.noreply.github.com>
2021-08-30 21:48:00 -0500

    Delete bonus.java

--- bonus.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class bonus{
-    public static void main (String args[]){
-        Scanner scan = new Scanner(System.in);
-        System.out.println("What is your name?");
-        String name = scan.nextLine();
-        System.out.println("Hello, "+name+"!");
-    }
-}
