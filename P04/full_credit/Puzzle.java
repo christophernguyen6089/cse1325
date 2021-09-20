a55062b43a431f58b00e063433be17288dc54fbc
christophernguyen6089 <christopher.nguyen10@mavs.uta.edu>
2021-09-20 18:44:41 -0500

    TODO: figure out what the heck is up with guesses[]

--- P04/full_credit/Puzzle.java
+++ P04/full_credit/Puzzle.java
@@ -4,19 +4,45 @@ public class Puzzle{
     
     public void Puzzle(String solution){
         this.solution = solution;
+        for(int h=0; h<solution.length(); h++){
+            this.guesses[h]=false;
+        }
     }
     public boolean guess(char c){
-    
+        boolean flag=false;
+        if(this.solution.indexOf(c)!=-1){
+            flag=true;
+            for(int i=0; i<solution.length(); i++){
+                if(c==solution.indexOf(i)){
+                    guesses[i]=true;
+                }
+            }
+        }
+        return flag;
     }
-    public void solve(String proposedSolution){
-    
+    public boolean solve(String proposedSolution){
+        if(proposedSolution.equals(this.solution)){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
     public String getSolution(){
         return solution;
     }
-    //the override goes... where?
+    @Override
     public String toString(){
-    
+        String str2 = "";
+        for(int j=0; j<solution.length(); j++){
+            if(this.guesses[j]==true){
+                System.out.print(solution.charAt(j));
+            }
+            else{
+                System.out.print("_");
+            }
+        }
+        return str2;
     }
     
 }
