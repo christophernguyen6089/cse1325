e9adceae73890bf9f05111bc56cb8fd2e35bdd56
christophernguyen6089 <christopher.nguyen10@mavs.uta.edu>
2021-09-27 12:03:52 -0500

    Last minute changes before commute

--- P05/full_credit/Store.java
+++ P05/full_credit/Store.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+
 public class Store{
     public static void cli(){
-    
+        ArrayList<Product> products = new ArrayList<Product>();
+            products.Taxfree.add(new Product("Milk",0.99));
+
     
     }
 
